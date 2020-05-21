528b984b1afb3c6310998f15107b15eb3db21d08
unknown <alextello1@hotmail.com>
2020-05-21 13:56:36 -0600

    Esctructura base la cual escucha y emite eventos de socket

---
+++ sockets/sockets.ts
@@ -0,0 +1,16 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+	cliente.on('disconnect', () => {
+		console.log('Cliente desconectado');
+	});
+};
+
+// Escuchar mensajes
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+	cliente.on('mensaje', (payload: { de: string; cuerpo: string }) => {
+		console.log('Se recibió el mensaje', payload);
+		io.emit('mensaje-nuevo', payload);
+	});
+};
